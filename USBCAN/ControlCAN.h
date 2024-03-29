deee3efc04f3d6db7c222986afc86fb64d20d446
Robot-Yue <a569768@126.com>
2024-03-29 14:08:47 +0800

    a

--- USBCAN/ControlCAN.h
+++ USBCAN/ControlCAN.h
@@ -5,11 +5,11 @@
 //#include <cvidef.h>	// 使用 CVI 平台开发，请使用该语句。
 
 // 接口卡类型定义
-#define VCI_USBCAN1	 3
-#define VCI_USBCAN2	 4
+#define VCI_USBCAN1  3
+#define VCI_USBCAN2  4
 #define VCI_USBCAN2A 4
 
-#define VCI_USBCAN_E_U 	20
+#define VCI_USBCAN_E_U  20
 #define VCI_USBCAN_2E_U 21
 
 #define UINT   unsigned int
